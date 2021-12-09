18ad9e9e57979723fef57d09df3244454cfa2a1d
unknown <markowujanovic@gmail.com>
2021-12-09 22:17:06 +0100

    remove loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { React } from 'react'
-import { Route, Switch } from 'react-router-loading'
+import { Route, Switch } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar.jsx'
 import HomePage from './components/HomePage/HomePage.jsx'
 import Footer from './components/Footer/Footer.jsx'
@@ -7,29 +7,19 @@ import CratesPage from './components/CratesPage/CratesPage.jsx'
 import ContactPage from './components/ContactPage/ContactPage.jsx'
 import AboutPage from './components/AboutPage/AboutPage.jsx'
 import NotMatch from './components/NotMatch/NotMatch.jsx'
-import { topbar } from "react-router-loading";
 
 function App() {
 
-  topbar.config({
-    autoRun: false,
-    barThickness: 4,
-    barColors: {
-        0: 'rgb(45, 194, 205)',
-        1.0: 'rgb(67, 92, 254)'
-    },
-    className: 'topbar'
-  });
 
   return (
     <>
       <Navbar />
-      <Switch maxLoadingTime={700}> 
-          <Route exact path="/" component={HomePage} loading />
-          <Route path="/about" component={AboutPage} loading />
-          <Route path="/crates" component={CratesPage} loading />
-          <Route path="/contact" component={ContactPage} loading />
-          <Route path="*" component={NotMatch} loading />
+      <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/crates" component={CratesPage}/>
+          <Route path="/contact" component={ContactPage}/>
+          <Route path="*" component={NotMatch}/>
       </Switch>
       <Footer />
   </>
