f94a76d815c4b617667bcefb9860d94c7bbc4f29
unknown <markowujanovic@gmail.com>
2022-02-13 12:26:46 +0100

    fix date

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 import './_footer.scss';
 import logo from '../../assets/logo.svg';
 
 const Footer = () => {
+
+    const [date, setDate] = useState('')
+
+
+    useEffect(() => {
+        setDate(new Date().getFullYear())
+    }, [])
 
     return (
        <footer>
            <div className="container">
                 <img src={logo} alt="nft" />
                 <p className="bold">Be part of the NFT gaming revolution.</p>
-                <p className="copy">© Copyright 2021 NFT2040:Arena. All rights reserved.</p>
+                <p className="copy">© Copyright {date} NFT2040:Arena. All rights reserved.</p>
            </div>
        </footer>
     )
