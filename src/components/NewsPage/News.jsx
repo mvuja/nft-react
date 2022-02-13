31c4eaddebec1e795b1bb9c7189fb5537317aa96
unknown <markowujanovic@gmail.com>
2022-02-13 23:13:52 +0100

    bug fix

--- src/components/NewsPage/News.jsx
+++ src/components/NewsPage/News.jsx
@@ -6,15 +6,25 @@ import arrow from '../../assets/arrow.svg'
 
 const News = props => {
     return (
+
         <section id="news">
-            <div className="container">
-            <img className="news-img" src={props.newsId.image} alt={props.newsId.title} />
-                <p className="news-date">{props.newsId.date}</p>
-                <h2 className="news-title">{props.newsId.title}</h2>
-                <p className="news-desc">{props.newsId.desc}</p>
-                <a className="news-link" href={`https://www.coindesk.com${props.newsId.url}`} target="_blank">See more <img src={arrow} alt="arrow" /></a>
+            {Object.keys(props.newsId).length !== 0 ?
+                <div className="container">
+                    <img className="news-img" src={props.newsId.image} alt={props.newsId.title} />
+                    <p className="news-date">{props.newsId.date}</p>
+                    <h2 className="news-title">{props.newsId.title}</h2>
+                    <p className="news-desc">{props.newsId.desc}</p>
+                    <a className="news-link" href={`https://www.coindesk.com${props.newsId.url}`} target="_blank">See more <img src={arrow} alt="arrow" /></a>
+                </div>
+            :
+            <div className="container container-error">
+                <h1>404 - PAGE NOT FOUND</h1>
             </div>
+            }
         </section>
+
+
+
     )
 }
 
